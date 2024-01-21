510eea7a46537e9982cf7970efd6c76208974b23
ArunMandal <marun1406@gmail.com>
2024-01-20 22:34:43 -0600

    Add project files.

---
+++ WebAPIWithReactProject/ClientApp/src/components/FetchEmployee.js
@@ -0,0 +1,32 @@
+﻿import React, { useEffect, useState } from 'react'
+
+const FetchEmployee = () => {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5008/api/employees')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setEmployees(data);
+            })
+            .catch((error) => console.log(error.message))
+    },[])
+
+    return (
+        <div>
+           
+            <h2> List  of Employee ha</h2>
+            <div>
+                {employees.map((x) => (
+                    <p key={x.employeeid}> {x.name} </p>
+                ))}
+            </div>
+
+            
+           
+        </div>                        
+    )
+}
+
+export default FetchEmployee
---
+++ WebAPIWithReactProject/Models/EmployManagementContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIWithReactProject.Models;
+
+public partial class EmployManagementContext : DbContext
+{
+    public EmployManagementContext()
+    {
+    }
+
+    public EmployManagementContext(DbContextOptions<EmployManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-B0CB314;Database=EmployManagement;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Employeeid).HasName("PK__EMPLOYEE__8F06515B78105C4E");
+
+            entity.ToTable("EMPLOYEE");
+
+            entity.Property(e => e.Employeeid).HasColumnName("EMPLOYEEID");
+            entity.Property(e => e.City)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("CITY");
+            entity.Property(e => e.Department)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("DEPARTMENT");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(6)
+                .IsUnicode(false)
+                .HasColumnName("GENDER");
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("NAME");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
